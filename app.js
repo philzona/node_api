84985db7e5eb712b92e69ce74bafec83275c9790
Phil Zona <phil.zona@codefresh.io>
2020-12-04 17:25:14 -0600

    Add first get route

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Working!' });
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// GET /tours
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
 });
 
 const port = process.env.PORT || 3000;
